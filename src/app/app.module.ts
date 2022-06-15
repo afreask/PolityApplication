631c11bcb9c019199038e23da1de71dafe384f5b
Paulo <paulo.valeza@gmail.com>
2022-06-15 16:38:18 -0600

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PageService } from './services/page.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/page.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PageService {
+
+  constructor(private http: HttpClient) { }
+
+  // An observable that is used to auto update every component that uses pages
+  public pagesBS = new BehaviorSubject<Object>(1);
+
+  getPages()
+  {
+      return this.http.get('https://localhost:44394/');
+  }
+
+}
