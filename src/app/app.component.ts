7027856749ea9a5026adf19b27bb09ce2984b2f6
Paulo <paulo.valeza@gmail.com>
2022-07-11 21:22:30 -0600

    Added the logout function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: String = 'title name';
+  user: any = JSON.parse(localStorage.getItem("user"));
+  successMsg: string;
+  page: any ;
+
+  constructor(
+
+    private router: Router
+  ) 
+  {
+    if(this.user != null)
+    {
+       this.page = this.user.pageList[this.user.pageList.length - 1]
+    }
+  }
+  logout(): void {
+    localStorage.removeItem('user');
+    this.router.navigateByUrl('user/login').then(() => {
+      window.location.reload();
+    });
+    // console.log(localStorage.getItem("user"))
+  }
 }
