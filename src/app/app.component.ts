983e268fa18d254670aa36eb8d33b5c759a60b30
Marimel (Office) <MarimelL@blairandco.ca>
2022-07-14 15:04:27 -0600

    Added an extra conditional to check if pageList.length is > 0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,20 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title: String = 'title name';
-  user: any = JSON.parse(localStorage.getItem("user"));
+  user: any = JSON.parse(localStorage.getItem('user'));
   successMsg: string;
-  page: any ;
+  page: any;
 
-  constructor(
-
-    private router: Router
-  ) 
-  {
-    if(this.user != null)
-    {
-       this.page = this.user.pageList[this.user.pageList.length - 1]
+  constructor(private router: Router) {
+    if (this.user != null && this.user.pageList.length > 0) {
+      this.page = this.user.pageList[this.user.pageList.length - 1];
     }
   }
+
   logout(): void {
     localStorage.removeItem('user');
     this.router.navigateByUrl('user/login').then(() => {
